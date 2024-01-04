44f2979f49434eae3d4ad4380a2248d6b218dae7
mrinalsinha2211 <145749474+mrinalsinha2211@users.noreply.github.com>
2024-01-04 12:01:57 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 const fs = require('fs').promises;
 const fileName = 'myfile.txt';
+const fileContent="Newton School";
 
 const updateFile = async (fileName, fileContent) => {
     //Write your code here to overwrite the file content
     //Don't change function name
-    
+     try{
+    const update=await fs.appendFile(fileName,fileContent);
+    console.log(`${fileName} is updated`);
+    return update
+  }catch (error){
+    console.log(`error in update${error.message}`);
+    return null;
+  }
 };
 
 
